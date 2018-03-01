013fccd5dcd5b47c09fea531b46faea95ad57721
Vincent ballut <vincent.ballut@backelite.com>
2018-03-01 17:36:43 +0100

    Refactors trailing slash in ApiConfig

--- src/scripts/config/api.config.js
+++ src/scripts/config/api.config.js
@@ -1,6 +1,6 @@
-export const WP_API_BASE_URL = 'http://wpreact.local/';
+export const WP_API_BASE_URL = 'http://wpreact.local';
 export const WP_API_ENDPOINTS = {
-  'pages': `${WP_API_BASE_URL}wp-json/wp/v2/pages`,
-  'posts': `${WP_API_BASE_URL}wp-json/wp/v2/posts`,
-  'menus': `${WP_API_BASE_URL}wp-json/menus/v1/menus`
+  'pages': `${WP_API_BASE_URL}/wp-json/wp/v2/pages`,
+  'posts': `${WP_API_BASE_URL}/wp-json/wp/v2/posts`,
+  'menus': `${WP_API_BASE_URL}/wp-json/menus/v1/menus`
 }
