f0200444352828cb311d957193e0104a4316eaa6
Vincent ballut <vincent.ballut@backelite.com>
2018-03-01 17:36:01 +0100

    Creates MenuModel

---
+++ src/scripts/models/menu.model.js
@@ -0,0 +1,53 @@
+export class Menu {
+  count;
+  description;
+  filter;
+  items;
+  name;
+  parent;
+  slug;
+  taxonomy;
+  term_group;
+  term_id;
+  term_taxonomy_id;
+};
+
+export class MenuItem {
+  ID;
+  attr_title;
+  classes;
+  comment_count;
+  comment_status;
+  db_id;
+  description;
+  filter;
+  guid;
+  menu_item_parent;
+  menu_order;
+  object;
+  object_id;
+  ping_status;
+  pinged;
+  post_author;
+  post_content;
+  post_content_filtered;
+  post_date;
+  post_date_gmt;
+  post_excerpt;
+  post_mime_type;
+  post_modified;
+  post_modified_gmt;
+  post_name;
+  post_parent;
+  post_password;
+  post_status;
+  post_title;
+  post_type;
+  target;
+  title;
+  to_ping;
+  type;
+  type_label;
+  url;
+  xfn;
+}
