b8cba978da2cf39a07242a189c0ad90635d5bfc2
Vincent ballut <vincent.ballut@backelite.com>
2018-03-15 10:11:54 +0100

    Creates SettingsActions and SettingsStore

---
+++ src/scripts/flux/actions/SettingsActions.js
@@ -0,0 +1,29 @@
+import axios                from 'axios';
+import alt                  from 'flux/alt/alt.js';
+import { WP_API_ENDPOINTS } from 'config/api.config.js';
+
+class SettingsActions {
+
+  fetchSettings() {
+    return (dispatch) => {
+      dispatch();
+
+      axios.get(WP_API_ENDPOINTS.settings).then((response) => { 
+        this.updateSettings(response.data);
+      }).catch((error) => {
+        this.fetchSettingsFailed(error);
+        throw error;
+      });
+    }
+  }
+  
+  updateSettings(data) {
+    return data;
+  }
+
+  fetchSettingsFailed(errorMessage) {
+    return errorMessage;
+  }
+}
+
+module.exports = alt.createActions(SettingsActions);
