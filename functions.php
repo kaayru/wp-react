f0a9be080162481a33a30ca70477e40990982408
Vincent ballut <vincent.ballut@backelite.com>
2018-03-15 10:28:31 +0100

    Adds support for custom logo

--- functions.php
+++ functions.php
@@ -77,6 +77,27 @@ function wpreact_setup() {
 
 	add_post_type_support( 'post', 'comments' );
 	add_post_type_support( 'page', 'comments' );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'wpreact_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/**
+	 * Add support for core custom logo.
+	 *
+	 * @link https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 250,
+		'width'       => 250,
+		'flex-width'  => true,
+		'flex-height' => true,
+	) );
 }
 endif; // wpreact_setup
 add_action( 'after_setup_theme', 'wpreact_setup' );
