91420849c6e64f61031bcd42916d802625375ead
Vincent ballut <vincent.ballut@backelite.com>
2018-03-15 10:16:57 +0100

    Inserts dynamic site name and description in header

--- src/scripts/components/header/header.js
+++ src/scripts/components/header/header.js
@@ -1,14 +1,37 @@
 import React                from 'react';
 import createReactClass     from 'create-react-class';
-import Navigation       from 'components/navigation/navigation.js';
+import Navigation           from 'components/navigation/navigation.js';
+import SettingsActions      from 'flux/actions/SettingsActions.js';
+import SettingsStore      from 'flux/stores/SettingsStore.js';
 
 var Header = createReactClass({  
+  getInitialState() {
+    return SettingsStore.getState();
+  },
+
+  componentDidMount() {
+    SettingsStore.listen(this.onChange);
+    SettingsActions.fetchSettings();
+  },
+
+  componentWillUnmount() {
+    SettingsStore.unlisten(this.onChange);
+  },
+
+  onChange(state) {
+    this.setState(state);
+  },
+
   render() {
+    if (!this.state.settings) {
+      return <header className="site-header"></header>
+    }
+
     return (
       <header className="site-header">
 		    <div className="site-header-branding">
-			    <h1 className="site-header-branding__title">Title</h1>
-				  <p className="site-header-branding__description">Just another WordPress site</p>
+			    <h1 className="site-header-branding__title">{ this.state.settings.name }</h1>
+				  <p className="site-header-branding__description">{ this.state.settings.description }</p>
         </div>
 
         <Navigation></Navigation>
