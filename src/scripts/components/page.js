545f6ab3f1c25419fc9d6c90c9bdc26954fe5863
Vincent ballut <vincent.ballut@backelite.com>
2018-04-09 10:01:22 +0200

    Removes DataStore import from pages

--- src/scripts/components/page.js
+++ src/scripts/components/page.js
@@ -1,18 +1,15 @@
 // @flow
 
-import DataStore from 'flux/stores/DataStore.js';
 import React     from 'react';
 
 class Page extends React.Component {
-    render() {
-        const allData = DataStore.getPageBySlug('home')
-
-        return (
-            <div>
-                <h2>Hello world! page</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Hello world! page</h2>
+      </div>
+    );
+  }
 }
 
 export default Page;
