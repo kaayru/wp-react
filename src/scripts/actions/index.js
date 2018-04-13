1ae8dff35ab697f6ce9d8a10934cb564d751a0ab
Vincent ballut <vincent.ballut@backelite.com>
2018-04-13 19:04:20 +0200

    Adds formatting code in menu action

--- src/scripts/actions/index.js
+++ src/scripts/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_SETTINGS = 'FETCH_SETTINGS';
 export const FETCH_MENU = 'FETCH_MENU';
 
 export function fetchSettings() {
-  const promise = axios.get(WP_API_ENDPOINTS.settings);
+  const promise = axios.get(WP_API_ENDPOINTS.settings)
 
   return {
     type: FETCH_SETTINGS,
@@ -14,7 +14,23 @@ export function fetchSettings() {
 }
 
 export function fetchMenu(menuId) {
-  const promise = axios.get(`${WP_API_ENDPOINTS.menus}/${menuId}`);
+  const promise = axios.get(`${WP_API_ENDPOINTS.menus}/${menuId}`)
+    .then(response => {
+
+      const getMenuItems = function(parentId, allMenuItems) {
+        return allMenuItems
+          .filter(item => item.menu_item_parent == parentId)
+          .map(item => {
+            item.items = getMenuItems(item.ID, allMenuItems);
+            return item;
+          });
+      }
+
+      const allMenuItems = response.data.items;
+      response.data.items = getMenuItems('0', allMenuItems)
+
+      return response;
+    });
 
   return {
     type: FETCH_MENU,
